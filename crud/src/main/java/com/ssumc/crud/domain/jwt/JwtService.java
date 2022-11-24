90737bd41df06ee1017024ce0553a36070a01f17
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-11-24 22:48:58 +0900

    ??∂ßå #3 JWT 생성중

---
+++ crud/src/main/java/com/ssumc/crud/domain/jwt/JwtService.java
@@ -0,0 +1,13 @@
+package com.ssumc.crud.domain.jwt;
+
+import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+
+public class JwtService {
+
+    public String createJwt(int userId) {
+        Date now = new Date();
+        return Jwts.
+    }
+}
