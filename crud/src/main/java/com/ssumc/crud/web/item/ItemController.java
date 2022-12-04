d8b3ade425dff332d3f9b9ffcd0b4ad8ed688949
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2022-12-04 18:11:39 +0900

    #2 ☑️ /items 이하 메소드 정리

--- crud/src/main/java/com/ssumc/crud/web/item/ItemController.java
+++ crud/src/main/java/com/ssumc/crud/web/item/ItemController.java
@@ -8,10 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -24,17 +21,21 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @ResponseBody
     @PostMapping(value = "/items/new")
-    public String addItem(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
+    public BaseResponse<Item> addItem(@RequestBody Item item) {
+
+        log.info(item.toString());
         Item item1 = new Item();
         item1.setStoreId(item.getStoreId());
         item1.setItemName(item.getItemName());
         item1.setItemPrice(item.getItemPrice());
         item1.setItemDetails(item.getItemDetails());
         item1.setItemPhotoUrl(item.getItemPhotoUrl());
+
         itemService.save(item);
 
-        return "redirect:/";
+        return new BaseResponse<>(item1 );
     }
 
 
